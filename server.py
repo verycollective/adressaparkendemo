14e4f66eb672aecc3112b5ce465cab83b9cfa21f
cv <cvaleonline@gmail.com>
2018-08-01 13:00:28 +0200

    fix formatting

--- server.py
+++ server.py
@@ -7,28 +7,35 @@ from lib.StupidArtnet import StupidArtnet
 
 @route('/')
 def menu():
+	"""Build html interface."""
 	return template('menu')
+
 
 @route('/sound/<speaker>')
 def sound(speaker):
+	"""Play demo sound in speaker."""
 	data, fs = sf.read('bell.wav', dtype='float32')
 
 	# To see available sound devices, use python3 -m sounddevice
 	sd.default.device = '24Ao'
-	#sd.default.device = 'Built-in Output' # OS x default
+	# sd.default.device = 'Built-in Output' # OS x default
 
-	mono = [(sample[0]+sample[1])/2 for sample in data]; #convert to mono
+	mono = [(sample[0] + sample[1]) / 2 for sample in data]  # convert to mono
 
 	speaker = int(speaker)
-	sd.play(mono, fs, mapping=[speaker], blocking=False) # remove mapping argument to test on machines without fancy soundcard
+	sd.play(mono, fs, mapping=[speaker], blocking=False)  # remove mapping to test without soundcard
 	return "OK"
+
 
 @route('/lightrandom/<address>')
 def lightrandom(address):
+	"""Set light to random colour."""
 	return lightsingle(address, randint(0, 255), randint(0, 255), randint(0, 255))
+
 
 @route('/lightall/<sr>/<sg>/<sb>')
 def allblue(sr, sg, sb):
+	"""Set all lights to blue."""
 	r = int(sr)
 	g = int(sg)
 	b = int(sb)
@@ -44,8 +51,10 @@ def allblue(sr, sg, sb):
 	a.set_rgb(91, r, g, b)
 	return "OK"
 
+
 @route('/lightallrandom')
 def allrandom():
+	"""Set all lights to random colours."""
 	a.set_rgb(1,  randint(0, 255), randint(0, 255), randint(0, 255))
 	a.set_rgb(11, randint(0, 255), randint(0, 255), randint(0, 255))
 	a.set_rgb(21, randint(0, 255), randint(0, 255), randint(0, 255))
@@ -58,23 +67,30 @@ def allrandom():
 	a.set_rgb(91, randint(0, 255), randint(0, 255), randint(0, 255))
 	return "OK"
 
+
 @route('/lightblackout')
 def blackout():
+	"""Set all lights off."""
 	a.blackout()
 	return "OK"
 
+
 @route('/lightsingle/<address>/<r>/<g>/<b>')
 def lightsingle(address, r, g, b):
-	address = int(address) # 1 - 11 - 21 - 31- 41 - 51 - 61 - 71 - 81 - 91
+	"""Set given RGB values to light."""
+	address = int(address)  # 1 - 11 - 21 - 31- 41 - 51 - 61 - 71 - 81 - 91
 
 	# SET THE ARTNET BUFFER TO OUR DATA ...
 	a.set_rgb(address, r, g, b)
 	return "OK"
 
+
 @route('/video')
 def video():
+	"""NYI."""
 	# todo show small video clip with hippo server
 	return "OK"
+
 
 # VALUES SET FOR THE ADRESSAPARKEN FIXED INSTALL
 target_ip = '192.168.1.10'  # typically in 2.x or 10.x range
